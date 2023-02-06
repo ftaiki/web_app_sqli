df8a28a18e506f4b6ead10b09a7c96b420db1059
taikif <hatakeyama.taiki1@gmail.com>
2023-02-06 14:19:24 +0900

    いらないクラスの排除

--- app/app.py
+++ app/app.py
@@ -12,27 +12,6 @@ app.config['USERNAME'] = 'admin'
 app.config['PASSWORD'] = 'password'
 app.config['SECRET_KEY'] = 'secret'
 
-class RequestFormatter(logging.Formatter):
-    def format(self, record):
-        if has_request_context():
-            record.url = request.url
-            record.remote_addr = request.remote_addr
-        else:
-            record.url = None
-            record.remote_addr = None
-
-        return super().format(record)
-
-formatter = RequestFormatter(
-    '[%(asctime)s] %(remote_addr)s requested %(url)s '
-    '%(levelname)s in %(module)s: %(message)s'
-)
-
-default_handler.setFormatter(formatter)
-default_handler.setLevel(logging.INFO)
-
-werkzeug_logger = logging.getLogger('werkzeug')
-werkzeug_logger.addHandler(default_handler)
 
 @app.route("/")
 def hello():
